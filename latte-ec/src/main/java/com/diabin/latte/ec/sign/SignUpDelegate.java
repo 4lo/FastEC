92c9b86f7de443ea7b0a6a055c710b79468cb2c4
4lo <wanziliang_4lo@foxmail.com>
2018-08-06 19:13:18 +0800

    登录界面/login

--- latte-ec/src/main/java/com/diabin/latte/ec/sign/SignUpDelegate.java
+++ latte-ec/src/main/java/com/diabin/latte/ec/sign/SignUpDelegate.java
@@ -52,6 +52,11 @@ public class SignUpDelegate extends LatteDelegate{
         }
     }
 
+    @OnClick(R2.id.tv_link_sign_in)
+    void onClickLink(){
+        start(new SignIndelegate());
+    }
+
     private boolean checkForm() {
         final String name = mName.getText().toString();
         final String email = mEmail.getText().toString();
